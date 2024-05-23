143baf584abc2cb51c91b3dcff0b4551c7245b2b
danielnudel <daniel.nudelman@mail.huji.ac.il>
2024-05-23 17:43:38 +0300

    Add schematic pages for single hist upload and for multiple hists for the same donor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.write("Choose your option from the left panel")
+
 def menu():
     st.sidebar.page_link("upload_single_hist.py", label="Single hist")
     st.sidebar.page_link("upload_multiple_hists_for_single_sample.py", label="Multiple hists hists for same donor")
