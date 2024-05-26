c9b92872fc799c20beb15dc388a2f4dcd5a69efe
Daniel Nudelman <tedy@cs.huji.ac.il>
2024-05-26 18:44:43 +0300

    Fix bug

--- pages/3_Upload_Cohort.py
+++ pages/3_Upload_Cohort.py
@@ -10,19 +10,19 @@ st.write("Upload multiple hists here")
 uploaded_files = st.file_uploader("Choose hist files", accept_multiple_files=True)
 summary = st.file_uploader('Upload summary file')
 
+if uploaded_files is not None and summary is not None:
+    files_d = {}
+    for file in uploaded_files:
+        files_d[file.name] = StringIO(file.getvalue().decode("utf-8"))
 
-files_d = {}
-for file in uploaded_files:
-    files_d[file.name] = StringIO(file.getvalue().decode("utf-8"))
-
-all_hists_by_sample_d = hist_cohort_pre_processing(files_d, StringIO(summary.getvalue().decode("utf-8")))
-for name in all_hists_by_sample_d:
-    ages = [name]
-    columns = ['sample_name']
-    for marker in all_hists_by_sample_d[name]:
-        hist = all_hists_by_sample_d[name][marker]
-        age, _, _, _, _ = predict(marker, pd.DataFrame(hist))
-        ages.append(age)
-        columns.append(marker)
-    df = pd.DataFrame([ages], columns=columns)
-    st.write(df)
+    all_hists_by_sample_d = hist_cohort_pre_processing(files_d, StringIO(summary.getvalue().decode("utf-8")))
+    for name in all_hists_by_sample_d:
+        ages = [name]
+        columns = ['sample_name']
+        for marker in all_hists_by_sample_d[name]:
+            hist = all_hists_by_sample_d[name][marker]
+            age, _, _, _, _ = predict(marker, pd.DataFrame(hist))
+            ages.append(age)
+            columns.append(marker)
+        df = pd.DataFrame([ages], columns=columns)
+        st.write(df)
