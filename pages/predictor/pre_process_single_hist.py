b99e8b1fca397c5f16bf0810ca50af2ca3492136
Daniel Nudelman <danielndelman@gmail.com>
2026-01-31 10:33:15 +0200

    Add logging to pre_process_single_hist.py Make both .hist and .CpG_hist files work.

--- pages/predictor/pre_process_single_hist.py
+++ pages/predictor/pre_process_single_hist.py
@@ -343,6 +343,7 @@ def hist_cohort_pre_processing(files, summary):
         sample_files = sample[1]['Sample #'].tolist()
         logger.debug(f'Sample files: {sample_files}')
         sample_files = ['Sample_' + str(num) + '_CpG.hist' for num in sample_files]
+        sample_files += ['Sample_' + str(num) + '.CpG_hist' for num in sample_files]
         sample_name = sample[0]
         files_for_sample = [files[file] for file in files if file in sample_files]
         logger.debug(f'Files for sample: {files_for_sample}')
